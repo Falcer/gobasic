c84df1ee78b129a3fda3efd65a6cd99c77945103
Argana Phangquestian <arganaphangquestian@gmail.com>
2020-03-14 11:35:01 +0700

    bla

--- controller/role.go
+++ controller/role.go
@@ -1,1 +1,48 @@
 package controller
+
+import "github.com/arganaphangquestian/gobasic/model"
+
+// GetAllRole handler
+func GetAllRole() model.APIResponse {
+	return model.APIResponse{
+		Status:  "success",
+		Message: "Get all role",
+		Data:    nil,
+	}
+}
+
+// GetAllByIDRole handler
+func GetAllByIDRole() model.APIResponse {
+	return model.APIResponse{
+		Status:  "success",
+		Message: "Get all role",
+		Data:    nil,
+	}
+}
+
+// CreateRole handler
+func CreateRole() model.APIResponse {
+	return model.APIResponse{
+		Status:  "success",
+		Message: "Get all role",
+		Data:    nil,
+	}
+}
+
+// UpdateRole handler
+func UpdateRole() model.APIResponse {
+	return model.APIResponse{
+		Status:  "success",
+		Message: "Get all role",
+		Data:    nil,
+	}
+}
+
+// DeleteRole handler
+func DeleteRole() model.APIResponse {
+	return model.APIResponse{
+		Status:  "success",
+		Message: "Get all role",
+		Data:    nil,
+	}
+}
